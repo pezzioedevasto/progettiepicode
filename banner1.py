110dc441df0c5c386f45ac029a3bba08b3aa2247
pezzioedevasto <132304866+pezzioedevasto@users.noreply.github.com>
2023-08-01 10:38:00 +0000

    banner linkedin

---
+++ banner1.py
@@ -0,0 +1,24 @@
+def main():
+    sfondo = ''' 
+   la foto del profilo è effettivamente una foto di profilo
+                                                                                                         _                                       MAIL:macri.michele.specialist@gmailcom
+                                                                                                       |  _ \\                                  Etical Hacker 
+                                                                                                       | |_) |
+                                                                                                       |  __/                                          
+                                                                                                       |_|                                             PENTESTING
+                                                                                                       |_|__
+                                                                                                          __ \\
+                                                                                                           _/ /
+                                                                                                           _ \\                                               E
+                                                                                                           _/ /
+                                                                                                           __/\\ 
+                                                                                                          |  _ \\
+                                                                                                          | | | |                                          DEFENSIVE
+                                                                                                          | |_| |                                           
+                                                                                                          |____/ 
+'''
+
+    print(sfondo)
+
+if __name__ == "__main__":
+    main()
